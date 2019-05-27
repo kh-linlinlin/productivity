db370eeb86e3dacdc0ecd678a3948ff000d527c3
JianaXu <50614413+JianaXu@users.noreply.github.com>
2019-05-27 18:28:37 +0800

    Add files via upload
    - changed to MySQL Database

--- input/forms.py
+++ input/forms.py
@@ -1,16 +1,21 @@
 from django import forms
 from input.models import Post
 from django.contrib.auth.models import User 
+task_list = ['Picking', 'Packing', 'Receiving', 'QC', 'Bundling', 'Putaway']
 
-
-class UserForm(forms.ModelForm):
+class ActionForm(forms.ModelForm):
 	user_name = forms.CharField()
 	task = forms.CharField(required = False)
 	work_complete = forms.CharField(required = False)
-
+	
 	class Meta:
 		model = Post
 		fields = ('user_name', 'task', 'work_complete')
+
+	def clean(self):
+		if self.cleaned_data['task'] not in task_list:
+			raise forms.ValidationError("This task is not valid. Please scan again!")
+		return self.cleaned_data  
 
 
 class GroupForm(forms.ModelForm):
--- web_proj/settings.py
+++ web_proj/settings.py
@@ -78,12 +78,24 @@ WSGI_APPLICATION = 'web_proj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'wpt',
+        'USER':'root',
+        'PASSWORD': 'Garena.com',
     }
 }
+
+        # 'HOST':'127.0.0.1',
+        # 'PORT':'3306'
 
 
 # Password validation
@@ -116,7 +128,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
