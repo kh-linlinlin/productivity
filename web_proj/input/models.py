40c245df585e2810d740087283652f101e0312ee
Lin Kehua <kehua.lin@sg.garena.com>
2019-06-21 19:01:01 +0800

    added dropdown

--- web_proj/input/models.py
+++ web_proj/input/models.py
@@ -6,3 +6,12 @@ class Post(models.Model):
 	wms_id = models.PositiveIntegerField()
 	task = models.CharField(max_length = 100)
 	user = models.ForeignKey(User, on_delete = models.CASCADE, null = True) 
+
+
+WHS_CHOICES = (
+    ('MYL','MYL'),
+    ('THA', 'THA')
+)
+
+class MyModel(models.Model):
+  color = models.CharField(max_length=6, choices=COLOR_CHOICES, default='MYL')
