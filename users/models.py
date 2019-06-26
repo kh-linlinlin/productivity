c6a3e32f293a3997342dca3fdaa4e8223c69db34
Lin Kehua <kehua.lin@sg.garena.com>
2019-06-26 14:48:20 +0800

    started with user profile improvements

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User 
 from django.utils import timezone
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
@@ -19,9 +21,19 @@ class Profile(models.Model):
 	ctime = models.DateTimeField(auto_now_add = True)
 	mime = models.DateTimeField(auto_now = True)
 
+
+
 	def __str__(self):
 		return f'{self.user.username}'
 
+# @receiver(post_save, sender=User)
+# def create_user_profile(sender, instance, created, **kwargs):
+# 	if created:
+# 		Profile.objects.create(user=instance)
+
+# @receiver(post_save, sender=User)
+# def save_user_profile(sender, instance, **kwargs):
+#     instance.profile.save()
 
 
 
--- users/urls.py
+++ users/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     url(r'^logout/$', auth_views.LogoutView.as_view(template_name = 'users/logout.html'), name='logout'),
     url(r'^register/$', views.register, name='register'),
     url(r'^userprofile/$', views.view_profile, name='view_profile'),
+    url(r'^upload/csv/$', views.upload_csv, name='upload_csv'),
     url(r'^userprofile/(?P<pk>\d+)/$', views.view_profile, name='view_profile_with_pk'),
 ]
